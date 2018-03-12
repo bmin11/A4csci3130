625b7692a35d0af36742d28e2c3147304e6e222d
bmin11 <br239881@gmail.com>
2018-03-12 17:08:32 -0300

    Change PrimaryBusiness input as Spinner and Add Firebase rules

--- app/src/main/java/com/acme/a3csci3130/CreateBusinessActivity.java
+++ app/src/main/java/com/acme/a3csci3130/CreateBusinessActivity.java
@@ -3,12 +3,15 @@ package com.acme.a3csci3130;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateBusinessActivity extends Activity {
 
-    private EditText etName, etNumber, etPrimaryBusiness, etAddress, etProvince;
+    private EditText etName, etNumber, etAddress, etProvince;
+    private Spinner spiPrimaryBusiness;
     private MyApplicationData appState;
 
     @Override
@@ -20,9 +23,14 @@ public class CreateBusinessActivity extends Activity {
 
         etName = (EditText) findViewById(R.id.etName);
         etNumber = (EditText) findViewById(R.id.etNumber);
-        etPrimaryBusiness = (EditText) findViewById(R.id.etPrimaryBusiness);
         etAddress = (EditText) findViewById(R.id.etAddress);
         etProvince = (EditText) findViewById(R.id.etProvince);
+
+        spiPrimaryBusiness = (Spinner) findViewById(R.id.spiPrimaryBusiness);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.primary_business_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiPrimaryBusiness.setAdapter(adapter);
     }
 
     public void submitInfoButton(View v) {
@@ -30,7 +38,7 @@ public class CreateBusinessActivity extends Activity {
         String uid = appState.firebaseReference.push().getKey();
         String name = etName.getText().toString();
         String number = etNumber.getText().toString();
-        String primaryBusiness = etPrimaryBusiness.getText().toString();
+        String primaryBusiness = spiPrimaryBusiness.getSelectedItem().toString();
         String address = etAddress.getText().toString();
         String province = etProvince.getText().toString();
         Business business = new Business(uid, name, number, primaryBusiness, address, province);
--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText etName, etNumber, etPrimaryBusiness, etAddress, etProvince;
+    private EditText etName, etNumber, etAddress, etProvince;
+    private Spinner spiPrimaryBusiness;
     Business receivedBusinessInfo;
     private MyApplicationData appState;
     private String mUid;
@@ -23,17 +26,26 @@ public class DetailViewActivity extends Activity {
 
         etName = (EditText) findViewById(R.id.etName);
         etNumber = (EditText) findViewById(R.id.etNumber);
-        etPrimaryBusiness = (EditText) findViewById(R.id.etPrimaryBusiness);
+        spiPrimaryBusiness = (Spinner) findViewById(R.id.spiPrimaryBusiness);
         etAddress = (EditText) findViewById(R.id.etAddress);
         etProvince = (EditText) findViewById(R.id.etProvince);
+
+        spiPrimaryBusiness = (Spinner) findViewById(R.id.spiPrimaryBusiness);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.primary_business_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiPrimaryBusiness.setAdapter(adapter);
 
         if(receivedBusinessInfo != null){
             etName.setText(receivedBusinessInfo.name);
             etNumber.setText(receivedBusinessInfo.number);
-            etPrimaryBusiness.setText(receivedBusinessInfo.primaryBusiness);
             etAddress.setText(receivedBusinessInfo.address);
             etProvince.setText(receivedBusinessInfo.province);
+
             mUid = receivedBusinessInfo.uid;
+
+            int pbID = adapter.getPosition(receivedBusinessInfo.primaryBusiness);
+            spiPrimaryBusiness.setSelection(pbID);
         }
     }
 
@@ -41,7 +53,7 @@ public class DetailViewActivity extends Activity {
         String uid = mUid;
         String name = etName.getText().toString();
         String number = etNumber.getText().toString();
-        String primaryBusiness = etPrimaryBusiness.getText().toString();
+        String primaryBusiness = spiPrimaryBusiness.getSelectedItem().toString();
         String address = etAddress.getText().toString();
         String province = etProvince.getText().toString();
         Business business = new Business(uid, name, number, primaryBusiness, address, province);
